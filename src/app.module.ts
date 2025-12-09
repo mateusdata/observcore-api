a13ab9614bd34333a5b7e228bd90e9dd556636d7
Mateus Santos <mateuspele2015@gmail.com>
2025-12-09 19:41:09 -0300

    feat: add throttling support and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './common/prisma/prisma.module';
-
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ThrottlerModule.forRoot({ throttlers: [{ ttl: 6000, limit: 20, }] }),
+    PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
