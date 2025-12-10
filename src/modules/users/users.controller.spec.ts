dbe1b046feba1d51240d44b333f8e480f51cdd5c
Mateus Santos <mateuspele2015@gmail.com>
2025-12-10 02:35:53 -0300

    feat: add PrismaService mock to user controller and service tests

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,14 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockPrismaService = {
+    user: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      findMany: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,15 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prisma: PrismaService;
+
+  const mockPrismaService = {
+    user: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      findMany: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
